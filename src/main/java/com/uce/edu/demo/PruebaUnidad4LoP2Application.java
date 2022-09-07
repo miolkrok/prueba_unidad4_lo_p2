1b511c8e1035070d166d84cf0b8af72f0871ed9d
Luis Ortiz <luchoaam97@gmail.com>
2022-09-06 20:41:33 -0500

    Prueba 4

--- src/main/java/com/uce/edu/demo/PruebaUnidad4LoP2Application.java
+++ src/main/java/com/uce/edu/demo/PruebaUnidad4LoP2Application.java
@@ -1,13 +1,18 @@
 package com.uce.edu.demo;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class PruebaUnidad4LoP2Application {
 
+	private static Logger LOG = Logger.getLogger(PruebaUnidad4LoP2Application.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaUnidad4LoP2Application.class, args);
+		LOG.info("log");
 	}
 
 }
