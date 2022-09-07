a887331e762225c71260e8ba066e1066a5d3db52
Luis Ortiz <luchoaam97@gmail.com>
2022-09-06 20:16:52 -0500

    Prueba 4

---
+++ src/test/java/com/uce/edu/demo/PruebaUnidad4LoP2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaUnidad4LoP2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
